0c80186258c5a5c28e07291956d2a23236d51ddf
OhMyGoodness <xelnagaaa@gmail.com>
2023-07-27 16:39:15 +0700

    Update AutoResource.php
    fix wrong class name phpdoc

--- laravel/app/Http/Resources/Auto/AutoResource.php
+++ laravel/app/Http/Resources/Auto/AutoResource.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
- * Class CityResource
+ * Class AutoResource
  * @package Http\Resources\Auto
  */
 class AutoResource extends JsonResource
